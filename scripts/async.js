4e0e5426a691aa49cb33d34baa9602593b9562d5
DHRUV KUMAR <dkiphones620@gmail.com>
2017-09-13 20:00:31 +0530

    added catch method to handle errors

--- scripts/async.js
+++ scripts/async.js
@@ -6,34 +6,39 @@ body.prepend(h1);
 let button = document.getElementById('Submit');
 
 
-button.addEventListener('click',function(e){
-e.preventDefault();
-fetchAndDisplay();
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    fetchAndDisplay();
 });
 
 
-function fetchAndDisplay(){
-	let keyword = document.getElementById('Enterkeyword').value
-	console.log(keyword);
-fetch(`https://api.unsplash.com/search/photos?page=1&query=${keyword}`, {
-    headers: {
-        Authorization: 'Client-ID d20594646ba6ccffeb7e0e80d4a8ebc7365248167aae2649c3684d77e23eafcd'
-    }
-}).then(response => response.json()).then(addImage);
+function fetchAndDisplay() {
+    let keyword = document.getElementById('Enterkeyword').value
+    console.log(keyword);
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${keyword}`, {
+        headers: {
+            Authorization: 'Client-ID d20594646ba6ccffeb7e0e80d4a8ebc7365248167aae2649c3684d77e23eafcd'
+        }
+    }).then(response => response.json()).then(addImage).catch(e => requestError(e, 'image'));
 
-function addImage(data) {
-    let htmlContent = '';
-    const firstImage = data.results[0];
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+    };
 
-    if (firstImage) {
-        htmlContent = `<figure>
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+
+        if (firstImage) {
+            htmlContent = `<figure>
             <img src="${firstImage.urls.small}" alt="${keyword}">
             <figcaption>${keyword} by ${firstImage.user.name}</figcaption>
         </figure>`;
-    } else {
-        htmlContent = 'Unfortunately, no image was returned for your search.'
-    }
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
 
-    body.insertAdjacentHTML('beforeend', htmlContent);
-}
+        body.insertAdjacentHTML('beforeend', htmlContent);
+    }
 }
